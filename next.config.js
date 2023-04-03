e0041844f6a0845a251b79bd83cc009ff90658a4
Sahil Satpute <explore.sahil@outlook.com>
2023-04-03 20:42:47 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repo = "iffi-mockup";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 module.exports = nextConfig;
